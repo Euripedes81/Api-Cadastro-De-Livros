89062ce552ac6a99d30c8accfcc60368b71cc754
Euripedes <euripedeslipin@gmail.com>
2021-11-20 18:44:45 -0300

    Ediçao documentação

--- ApiCadastroDeLivros/Controllers/AutorController.cs
+++ ApiCadastroDeLivros/Controllers/AutorController.cs
@@ -20,6 +20,16 @@ namespace ApiCadastroDeLivros.Controllers
         {
             _autorService = autorService;
         }
+        /// <summary>
+        /// Buscar todos os Autores de forma paginada
+        /// </summary>
+        /// <remarks>
+        /// Não é possível retornar os Autores sem paginação
+        /// </remarks>
+        /// <param name="pagina">Indica qual página está sendo consultada. Mínimo 1</param>
+        /// <param name="quantidade">Indica a quantidade de registros por página. Mínimo 1 e máximo 50</param>
+        /// <response code="200">Retorna a lista de Autores</response>
+        /// <response code="204">Caso não haja Autores</response>   
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AutorViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
         {
@@ -30,7 +40,13 @@ namespace ApiCadastroDeLivros.Controllers
 
             return Ok(autores);
         }
-        
+
+        /// <summary>
+        /// Buscar um Autor pelo seu Id
+        /// </summary>
+        /// <param name="idAutor">Id do Autor buscado</param>
+        /// <response code="200">Retorna o Autor filtrado</response>
+        /// <response code="204">Caso não haja Autor com este id</response>   
         [HttpGet("{idAutor}")]
         public async Task<ActionResult<AutorViewModel>> Obter([FromRoute] int idAutor)
         {
@@ -41,6 +57,13 @@ namespace ApiCadastroDeLivros.Controllers
             }
             return Ok(autor);
         }
+
+        /// <summary>
+        /// Inserir um Autor no catálogo
+        /// </summary>
+        /// <param name="autorInputModel">Dados do Autor a ser inserido</param>
+        /// <response code="200">Cao o Autor seja inserido com sucesso</response>
+        /// <response code="422">Caso já exista um Autor com mesmo nome para a mesma produtora</response>   
         [HttpPost]
         public async Task<ActionResult<AutorViewModel>> InserirAutor([FromBody] AutorInputModel autorInputModel)
         {
@@ -54,12 +77,20 @@ namespace ApiCadastroDeLivros.Controllers
                 return UnprocessableEntity("Livro existente ou Id do Autor inválida!");
             }
         }
+
+        /// <summary>
+        /// Atualizar um Autor no catálogo
+        /// </summary>
+        /// /// <param name="idAutor">Id do Autor a ser atualizado</param>
+        /// <param name="autorInputModel">Novos dados para atualizar o Autor indicado</param>
+        /// <response code="200">Caso o Autor seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um Autor com este Id</response>   
         [HttpPut("{idAutor}")]
-        public async Task<ActionResult> AtualizarLivro([FromRoute] int idAutor, [FromBody] AutorInputModel autor)
+        public async Task<ActionResult> AtualizarLivro([FromRoute] int idAutor, [FromBody] AutorInputModel autorInputModel)
         {
             try
             {
-                await _autorService.Atualizar(idAutor, autor);
+                await _autorService.Atualizar(idAutor, autorInputModel);
                 return Ok();
             }
             catch (Exception)
@@ -68,7 +99,13 @@ namespace ApiCadastroDeLivros.Controllers
                 return NotFound("Não existe este Autor!");
             }
         }
-        
+
+        /// <summary>
+        /// Excluir um Autor
+        /// </summary>
+        /// /// <param name="idAutor">Id do Autor a ser excluído</param>
+        /// <response code="200">Caso o Autor seja excluído com sucesso</response>
+        /// <response code="404">Caso não exista um Autor com este Id</response>   
         [HttpDelete("{idAutor}")]
         public async Task<ActionResult> ApagarAutor([FromRoute] int idAutor)
         {
--- ApiCadastroDeLivros/Controllers/LivroController.cs
+++ ApiCadastroDeLivros/Controllers/LivroController.cs
@@ -20,7 +20,17 @@ namespace ApiCadastroDeLivros.Controllers
         {
             _livroService = livroService;
         }
-       
+
+        /// <summary>
+        /// Buscar todos os Livros de forma paginada
+        /// </summary>
+        /// <remarks>
+        /// Não é possível retornar os Livros sem paginação
+        /// </remarks>
+        /// <param name="pagina">Indica qual página está sendo consultada. Mínimo 1</param>
+        /// <param name="quantidade">Indica a quantidade de registros por página. Mínimo 1 e máximo 50</param>
+        /// <response code="200">Retorna a lista de Livros</response>
+        /// <response code="204">Caso não haja Livros</response>   
         [HttpGet]
         public async Task<ActionResult<IEnumerable<LivroViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
         {
@@ -31,7 +41,13 @@ namespace ApiCadastroDeLivros.Controllers
 
             return Ok(livros);
         }
-        
+
+        /// <summary>
+        /// Buscar um Livro pelo seu Id
+        /// </summary>
+        /// <param name="idLivro">Id do Livro buscado</param>
+        /// <response code="200">Retorna o Livro filtrado</response>
+        /// <response code="204">Caso não haja Livro com este id</response>   
         [HttpGet("{idLivro}")]
         public async Task<ActionResult<LivroViewModel>> Obter([FromRoute] int idLivro)
         {
@@ -42,7 +58,13 @@ namespace ApiCadastroDeLivros.Controllers
             }
             return Ok(livro);
         }
-        
+
+        /// <summary>
+        /// Inserir um Livro no catálogo
+        /// </summary>
+        /// <param name="livroInputModel">Dados do Livro a ser inserido</param>
+        /// <response code="200">Cao o Livro seja inserido com sucesso</response>
+        /// <response code="422">Caso já exista um Livro com mesmo nome para a mesma produtora</response>   
         [HttpPost]
         public async Task<ActionResult<LivroViewModel>> InserirLivro([FromBody] LivroInputModel livroInputModel)
         {
@@ -56,13 +78,20 @@ namespace ApiCadastroDeLivros.Controllers
                 return UnprocessableEntity("Livro existente ou Id do Autor inválida!");
             }
         }
-        
+
+        /// <summary>
+        /// Atualizar um Livro no catálogo
+        /// </summary>
+        /// /// <param name="idLivro">Id do Livro a ser atualizado</param>
+        /// <param name="livroInputModel">Novos dados para atualizar o Livro indicado</param>
+        /// <response code="200">Cao o Livro seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um Livro com este Id</response>   
         [HttpPut("{idLivro}")]
-        public async Task<ActionResult> AtualizarLivro ([FromRoute] int idLivro, [FromBody] LivroInputModel livro)
+        public async Task<ActionResult> AtualizarLivro ([FromRoute] int idLivro, [FromBody] LivroInputModel livroInputModel)
         {
             try
             {
-               await _livroService.Atualizar(idLivro, livro);
+               await _livroService.Atualizar(idLivro, livroInputModel);
                 return Ok();
             }
             catch (Exception)
@@ -72,10 +101,11 @@ namespace ApiCadastroDeLivros.Controllers
             }
         }
         /// <summary>
-        /// 
+        /// Excluir um Livro
         /// </summary>
-        /// <param name="idLivro"></param>
-        /// <returns></returns>
+        /// /// <param name="idLivro">Id do Livro a ser excluído</param>
+        /// <response code="200">Caso o Livro seja excluído com sucesso</response>
+        /// <response code="404">Caso não exista um Livro com este Id</response>   
         [HttpDelete("{idLivro}")]
         public async Task<ActionResult> ApagarLivro([FromRoute] int idLivro)
         {
